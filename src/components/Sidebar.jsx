d8c015ed8819bb085050355b4e75e9e5bbe38896
Sid Parekar <sparekar@amazon.com>
2022-10-05 14:40:25 -0700

    handle sidebar for mmobile screens

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
+import { HiOutlineMenu } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri"
 import { links } from "../assets/constants";
 import { logo } from "../assets";
 import { useState } from "react";
 
-const NavLinks=()=> (
+const NavLinks=({handleClick})=> (
     <div className="mt-10">
       {links.map((item)=>(
         <NavLink
@@ -12,7 +13,9 @@ const NavLinks=()=> (
         to={item.to}
         className='flex flex-row justify-starts items-center my-8 text-sm
         font-medium text-gray-400 hover:text-cyan-400'
+        onClick={()=> handleClick && handleClick() }//we can call this only on MOBILE devices when it is available
         >
+          <item.icon className="w-6 h-6 mr-2"/>
           {item.name}
         </NavLink>
         ))}
@@ -32,6 +35,19 @@ const Sidebar = () => {
     py-10 px-4 bg-[#191624]">
       <img src={logo} atl="logo" className="w-full h-14 object-contain"/>
       <NavLinks/>
+    </div>
+
+    <div className="absolute md:hidden block top-6 right-3">
+      {mobileMenuOpen ? 
+      <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)}/> :
+      <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)}/>
+      }
+    </div>
+    <div className={`absolute top-0 h-screen w-2/3
+        bg-gradient-to-t1 from-white/10 to-[#483d8b] backdrop-blur-lg 
+        z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <img src={logo} atl="logo" className="w-full h-14 object-contain"/>
+      <NavLinks handleClick= {() => setMobileMenuOpen(false)}/>
     </div>
     </>
   )
